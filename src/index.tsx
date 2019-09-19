5d71dee2b3d6e6b034a63e955cefe222ecfd99f6
yuri <yuri.haruta@gmail.com>
2019-09-19 17:58:29 +0200

    add a store

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,28 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+import { createStore } from 'redux';
+import { enthusiasm } from './reducers/index';
+import { StoreState } from './types/index';
+import Hello from './containers/Hello';
+import { Provider } from 'react-redux';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore<StoreState>(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: 'TypeScript',
+});
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Hello />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
-registerServiceWorker();
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root') as HTMLElement
+// );
+// registerServiceWorker();
